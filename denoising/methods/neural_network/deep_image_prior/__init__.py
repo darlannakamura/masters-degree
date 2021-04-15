6a436d24ea03694434afd772e53e0ae1b4fe26e6
Darlan Nakamura <darlannakamura@hotmail.com>
2021-04-15 07:30:35 -0300

    fix: adjust DeepImagePrior class Add run() method, to receive a single image and return two filtered images

--- denoising/methods/neural_network/deep_image_prior/__init__.py
+++ denoising/methods/neural_network/deep_image_prior/__init__.py
@@ -1,14 +1,8 @@
+import numpy as np
 import torch
 import torch.optim
 
-# from skimage import metrics
-# import metrics.peak_signal_to_noise_ratio as compare_psnr
-# import metrics.structural_similarity as compare_ssim
-# from skimage.metrics import peak_signal_to_noise_ratio as compare_psnr
-# from skimage.metrics import structural_similarity as compare_ssim
-
 from skimage.measure import compare_psnr, compare_ssim
-from skimage.util import random_noise
 
 from .models import *
 from .utils.denoising_utils import *
@@ -18,24 +12,8 @@ torch.backends.cudnn.benchmark =True
 dtype = torch.cuda.FloatTensor
 
 class DeepImagePrior:
-    def __init__(self, iterations, filename, sigma=25/255., noise='poisson'):
-        imsize =-1
-        PLOT = False
-
-        # Add synthetic noise
-        img_pil = crop_image(get_image(filename, imsize)[0], d=32)
-        img_np = pil_to_np(img_pil)
-
-        # Add synthetic noise
-        img_pil = crop_image(get_image(filename, imsize)[0], d=32)
-        img_np = pil_to_np(img_pil)
-
-        #     img_noisy_pil, img_noisy_np = get_noisy_image(img_np, sigma_)
-        img_noisy_np = random_noise(img_np, mode='poisson').astype(np.float32)
-        img_noisy_pil = np_to_pil(img_noisy_np)
-            
-        if PLOT:
-            plot_image_grid([img_np, img_noisy_np], 4, 6);
+    def run(self, iterations: int, image_noisy: np.ndarray):
+        # assert len(image_noisy.shape) == 2, "image_noisy should have 2 dimensions."
 
         INPUT = 'noise' # 'meshgrid'
         pad = 'reflection'
@@ -48,10 +26,12 @@ class DeepImagePrior:
         show_every = 100
         exp_weight=0.99
 
+        img_noisy_np = image_noisy
+        img_noisy_pil = np_to_pil(img_noisy_np)
+        
         num_iter = iterations
         input_depth = 1 
-        figsize = 4 
-
+        
         net = get_net(input_depth, 'skip', pad,
                         skip_n33d=128, 
                         skip_n33u=128, 
@@ -59,7 +39,7 @@ class DeepImagePrior:
                         num_scales=5,
                         upsample_mode='bilinear').type(dtype)
 
-        net_input = get_noise(input_depth, INPUT, (img_pil.size[1], img_pil.size[0])).type(dtype).detach()
+        net_input = get_noise(input_depth, INPUT, (img_noisy_pil.size[1], img_noisy_pil.size[0])).type(dtype).detach()
         print(net_input.shape)
         print(img_noisy_np.shape)
 
@@ -102,17 +82,11 @@ class DeepImagePrior:
             total_loss.backward()
 
             psrn_noisy = compare_psnr(img_noisy_np, out.detach().cpu().numpy()[0]) 
-            psrn_gt    = compare_psnr(img_np, out.detach().cpu().numpy()[0]) 
-            psrn_gt_sm = compare_psnr(img_np, self.out_avg.detach().cpu().numpy()[0]) 
             
             # Note that we do not have GT for the "snail" example
             # So 'PSRN_gt', 'PSNR_gt_sm' make no sense
-            print ('Iteration %05d    Loss %f   PSNR_noisy: %f   PSRN_gt: %f PSNR_gt_sm: %f' % (self.i, total_loss.item(), psrn_noisy, psrn_gt, psrn_gt_sm), '\r', end='')
-            if  PLOT and self.i % show_every == 0:
-                out_np = torch_to_np(out)
-                plot_image_grid([np.clip(out_np, 0, 1), 
-                                np.clip(torch_to_np(self.out_avg), 0, 1)], factor=figsize, nrow=1)
-
+            print ('Iteration %05d    Loss %f   PSNR_noisy: %f    ' % (self.i, total_loss.item(), psrn_noisy), '\r', end='')
+            
             # Backtracking
             if self.i % show_every:
                 if psrn_noisy - self.psrn_noisy_last < -5: 
@@ -139,13 +113,4 @@ class DeepImagePrior:
         out_np_avg = torch_to_np(self.out_avg)
         out_np = torch_to_np(net(self.net_input))
 
-        q = plot_image_grid([np.clip(out_np_avg, 0, 1), np.clip(out_np, 0, 1), img_np], factor=13);
-        print('PSNR: ', compare_psnr(img_np, out_np_avg))
-        print('PSNR: ', compare_psnr(img_np, out_np))
-
-        img_np = np.squeeze(np.moveaxis(img_np[...,0],0,-1))
-        out_np = np.squeeze(np.moveaxis(out_np[...,0],0,-1))
-        out_np_avg = np.squeeze(np.moveaxis(out_np_avg[...,0],0,-1))
-
-        print('SSIM: ', compare_ssim(img_np, out_np))
-        print('SSIM: ', compare_ssim(img_np, out_np_avg))
+        return out_np, out_np_avg
--- denoising/methods/neural_network/deep_image_prior/tests/test_dip.py
+++ denoising/methods/neural_network/deep_image_prior/tests/test_dip.py
@@ -1,12 +1,36 @@
 import os, unittest
+import numpy as np
+from skimage.measure import compare_psnr, compare_ssim
+from skimage.util import random_noise
 
 from denoising.methods.neural_network.deep_image_prior import DeepImagePrior
+from denoising.methods.neural_network.deep_image_prior.utils.denoising_utils import *
+
+from settings import BASE_DIR
 
 class TestDeepImagePrior(unittest.TestCase):
     def test_improvement_with_deep_image_prior(self):
-        DeepImagePrior(iterations=100, 
-            filename='/media/darlan/DATA3/deeplearning-projects/prisma-library/denoising/methods/neural_network/deep_image_prior/data/cameraman.png'
-        )
+        filename = os.path.join(BASE_DIR, 'denoising', 'methods', 'neural_network', 'deep_image_prior', 'data', 'cameraman.png')
+        
+        # Add synthetic noise
+        img_pil = crop_image(get_image(filename, -1)[0], d=32)
+        img_np = pil_to_np(img_pil)
 
+        img_noisy_np = random_noise(img_np, mode='poisson').astype(np.float32)
+
+        dip = DeepImagePrior()
+
+        out, out_avg = dip.run(iterations=10, image_noisy=img_noisy_np)
+
+        self.assertTrue(compare_psnr(img_np, out) > 10)
+        self.assertTrue(compare_psnr(img_np, out_avg) > 10)
+
+        img_np = np.squeeze(np.moveaxis(img_np[...,0],0,-1))
+        out_np = np.squeeze(np.moveaxis(out[...,0],0,-1))
+        out_np_avg = np.squeeze(np.moveaxis(out_avg[...,0],0,-1))
+
+        self.assertTrue(compare_ssim(img_np, out_np) > 0.50)
+        self.assertTrue(compare_ssim(img_np, out_np_avg) > 0.50)
+        
 if __name__ == '__main__':
     unittest.main()
