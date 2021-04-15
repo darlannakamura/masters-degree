5223e4dbf850d8afaf4b38f930a5e0c450d90be9
Darlan Nakamura <darlannakamura@hotmail.com>
2021-04-15 08:54:42 -0300

    feat: add deep image prior wrapper

--- denoising/methods/neural_network/deep_image_prior/__init__.py
+++ denoising/methods/neural_network/deep_image_prior/__init__.py
@@ -1,116 +1,32 @@
 import numpy as np
-import torch
-import torch.optim
 
-from skimage.measure import compare_psnr, compare_ssim
+from denoising.methods.neural_network.deep_image_prior.main import DeepImagePrior
+from denoising.utils import *
 
-from .models import *
-from .utils.denoising_utils import *
 
-torch.backends.cudnn.enabled = True
-torch.backends.cudnn.benchmark =True
-dtype = torch.cuda.FloatTensor
+def deep_image_prior(noisy_images: np.ndarray, iterations=1000) -> np.ndarray:
+    validate_array_input(noisy_images)
 
-class DeepImagePrior:
-    def run(self, iterations: int, image_noisy: np.ndarray):
-        # assert len(image_noisy.shape) == 2, "image_noisy should have 2 dimensions."
+    filtered_images = []
 
-        INPUT = 'noise' # 'meshgrid'
-        pad = 'reflection'
-        OPT_OVER = 'net' # 'net,input'
+    dip = DeepImagePrior()
 
-        reg_noise_std = 1./30. # set to 1./20. for sigma=50
-        LR = 0.01
+    # convert (52,52,1) to (1,52,52)
+    noisy_images = noisy_images[:, :, :, 0]
+    noisy_images = noisy_images.reshape((1,) + noisy_images.shape)
 
-        OPTIMIZER='adam' # 'LBFGS'
-        show_every = 100
-        exp_weight=0.99
+    noisy_images = normalize(noisy_images, interval=(0,1), data_type='float')
 
-        img_noisy_np = image_noisy
-        img_noisy_pil = np_to_pil(img_noisy_np)
-        
-        num_iter = iterations
-        input_depth = 1 
-        
-        net = get_net(input_depth, 'skip', pad,
-                        skip_n33d=128, 
-                        skip_n33u=128, 
-                        skip_n11=4, 
-                        num_scales=5,
-                        upsample_mode='bilinear').type(dtype)
+    for i in range(noisy_images.shape[1]):
+        img = noisy_images[:, i, :, :]
+        predicted_image, predicted_image_avg = dip.run(
+            iterations=iterations,
+            image_noisy=img
+        )
 
-        net_input = get_noise(input_depth, INPUT, (img_noisy_pil.size[1], img_noisy_pil.size[0])).type(dtype).detach()
-        print(net_input.shape)
-        print(img_noisy_np.shape)
+        predicted_image = predicted_image[0, :, :]
 
-        # Compute number of parameters
-        s  = sum([np.prod(list(p.size())) for p in net.parameters()]); 
-        print ('Number of params: %d' % s)
-
-        # Loss
-        mse = torch.nn.MSELoss().type(dtype)
-
-        img_noisy_torch = np_to_torch(img_noisy_np).type(dtype)
-
-        print(img_noisy_torch.shape)
-
-        net_input_saved = net_input.detach().clone()
-        noise = net_input.detach().clone()
-        self.out_avg = None
-        self.last_net = None
-        self.psrn_noisy_last = 0
-        self.net_input = net_input
-        self.i = 0
-        
-        def closure():
-            if reg_noise_std > 0: #reg_noise_std == 1./30
-                self.net_input = net_input_saved + (noise.normal_() * reg_noise_std)
-                #Ou seja, vamos a cada iteração tentar deixar o ruído cada vez mais parecido com o
-                #que acreditamos ser o ruído real.
-
-            out = net(self.net_input)
-            
-            # Smoothing
-            if self.out_avg is None:
-                self.out_avg = out.detach()
-            else:
-                #vamos a cada iteração levar em consideração 99% do anterior e 
-                #1% da imagem de saída atual
-                self.out_avg = self.out_avg * exp_weight + out.detach() * (1 - exp_weight)
-                
-            total_loss = mse(out, img_noisy_torch)
-            total_loss.backward()
-
-            psrn_noisy = compare_psnr(img_noisy_np, out.detach().cpu().numpy()[0]) 
-            
-            # Note that we do not have GT for the "snail" example
-            # So 'PSRN_gt', 'PSNR_gt_sm' make no sense
-            print ('Iteration %05d    Loss %f   PSNR_noisy: %f    ' % (self.i, total_loss.item(), psrn_noisy), '\r', end='')
-            
-            # Backtracking
-            if self.i % show_every:
-                if psrn_noisy - self.psrn_noisy_last < -5: 
-                    print('Falling back to previous checkpoint.')
-
-                    for new_param, net_param in zip(self.last_net, net.parameters()):
-                        net_param.data.copy_(new_param.cuda())
-
-                    return total_loss*0
-                else:
-                    self.last_net = [x.detach().cpu() for x in net.parameters()]
-                    self.psrn_noisy_last = psrn_noisy
-                    
-            self.i += 1
-
-            return total_loss
-
-        p = get_params(OPT_OVER, net, self.net_input)
-        optimize(OPTIMIZER, p, closure, LR, num_iter)
-
-        p = get_params(OPT_OVER, net, self.net_input)
-        optimize(OPTIMIZER, p, closure, LR, num_iter)
-
-        out_np_avg = torch_to_np(self.out_avg)
-        out_np = torch_to_np(net(self.net_input))
-
-        return out_np, out_np_avg
+        filtered_images.append(predicted_image)
+    
+    filtered_images = np.array(filtered_images)
+    return filtered_images.reshape(filtered_images.shape + (1,))
--- denoising/methods/neural_network/deep_image_prior/tests/test_dip.py
+++ denoising/methods/neural_network/deep_image_prior/tests/test_dip.py
@@ -1,12 +1,24 @@
-import os, unittest
+import os, unittest, math
 import numpy as np
 from skimage.measure import compare_psnr, compare_ssim
 from skimage.util import random_noise
 
-from denoising.methods.neural_network.deep_image_prior import DeepImagePrior
+from denoising.methods.neural_network.deep_image_prior.main import DeepImagePrior
+from denoising.methods.neural_network.deep_image_prior import deep_image_prior
+
 from denoising.methods.neural_network.deep_image_prior.utils.denoising_utils import *
 
-from settings import BASE_DIR
+from settings import BASE_DIR, BSD300_DIR
+
+from denoising.datasets import load_bsd300
+from denoising.datasets import load_dataset
+from denoising.datasets import extract_patches
+from denoising.datasets import add_noise
+
+from denoising.metrics import psnr, ssim
+
+from denoising.utils import normalize
+
 
 class TestDeepImagePrior(unittest.TestCase):
     def test_improvement_with_deep_image_prior(self):
@@ -32,5 +44,33 @@ class TestDeepImagePrior(unittest.TestCase):
         self.assertTrue(compare_ssim(img_np, out_np) > 0.50)
         self.assertTrue(compare_ssim(img_np, out_np_avg) > 0.50)
         
+class TestDeepImagePriorWrapper(unittest.TestCase):
+    def load_data(self):
+        imgs = load_bsd300(BSD300_DIR)
+        patches = extract_patches(imgs, begin=(0,0), stride=10,
+            dimension=(52,52), quantity_per_image=(5,5))
+    
+        _, y_test = load_dataset(patches, shuffle=False, split=(80,20))
+
+        self.mean = 0.0
+        self.variance = 0.01
+        self.std = math.sqrt(self.variance) # 0.1
+
+        x_test = add_noise(y_test, noise='gaussian', mean=self.mean, var=self.variance)
+
+        x_test = x_test.astype('float32') / 255.0
+
+        y_test = y_test.astype('float32') / 255.0
+
+        return (x_test, y_test)
+
+    def test_deep_image_prior_wrapper(self):
+        (x_test, y_test) = self.load_data()
+
+        pred = deep_image_prior(x_test[:2], iterations=10)
+
+        self.assertTrue(psnr(normalize(y_test[:2], data_type='int'), normalize(pred, data_type='int')).mean() > 10)
+        
+
 if __name__ == '__main__':
     unittest.main()
