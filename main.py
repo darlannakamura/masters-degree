410d8e8583239859c2813a169f9aa2100c701083
Darlan Nakamura <darlannakamura@hotmail.com>
2021-04-19 08:55:46 -0300

    feat: initializing argparser

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from experiments import Experiment
+import argparse
 
-e = Experiment()
-e.run()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run your denoising experiments.')
+    parser.add_argument('-f', '--file', type=str, required=True, help='experiment configuration file')
+
+    args = parser.parse_args()
+
+    e = Experiment(filename=args.file)
+    e.run()
