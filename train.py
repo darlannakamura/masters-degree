f4527873ea2df8be229cf4d3526feb8f8b391c3e
Darlan Nakamura <darlannakamura@hotmail.com>
2021-05-01 14:01:19 -0300

    fix: fix train script

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os
+import time
 import yaml
 import numpy as np
 import math
@@ -8,6 +9,10 @@ from multiprocessing import Process
 from typing import Tuple, Dict
 
 EPOCHS = 40
+
+def append_in_csv(filename: str, row: str):
+    with open(filename, 'a') as f:
+        f.write(row+'\n')
 
 def load_data(check: bool) -> Tuple[np.ndarray]:
     from settings import BASE_DIR, BSD300_DIR
@@ -60,11 +65,11 @@ def load_config(file: str) -> Dict[str, str]:
 def get_epochs(check):
     return EPOCHS if not check else 1
 
-def train_mlp(file: str, check: bool):
+def train_mlp(file: str, check: bool, config: Dict[str, str]):
+    start_time = time.time()
     from denoising.methods.neural_network.mlp import MLP
 
     (x_train, y_train, _, _) = load_data(check)
-    config = load_config(file)
 
     mlp = MLP(image_dimension=(52,52), hidden_layers=3, depth=32, multiply=True)
     mlp.compile(optimizer="adam", learning_rate=0.0001, loss="mse")
@@ -72,15 +77,21 @@ def train_mlp(file: str, check: bool):
     ckpt = os.path.join(config['metadata_path'], f'MLP.hdf5')
     mlp.set_checkpoint(filename=ckpt, save_best_only=True, save_weights_only=False)
 
-    mlp.fit(epochs=get_epochs(check), batch_size=128, shuffle=True, extract_validation_dataset=True)
+    mlp.fit(epochs=get_epochs(check), batch_size=128, shuffle=True, 
+        x_train=x_train, y_train=y_train,
+        extract_validation_dataset=True)
+
+    time_in_seconds = time.time() - start_time
 
     mlp.save_loss_plot(os.path.join(config['output_path'], f'mlp_loss.png'))
+    
+    append_in_csv(config['time_file'], f'mlp, {time_in_seconds}')
 
-def train_cnn(file: str, check: bool):
+def train_cnn(file: str, check: bool, config: Dict[str, str]):
+    start_time = time.time()
     from denoising.methods.neural_network.cnn import CNN
 
     (x_train, y_train, _, _) = load_data(check)
-    config = load_config(file)
 
     cnn = CNN(image_dimension=(52,52), hidden_layers=10, depth=32, multiply=False, pooling=None)
     cnn.compile(optimizer="adam", learning_rate=0.0001, loss='mse')
@@ -89,11 +100,17 @@ def train_cnn(file: str, check: bool):
     cnn.set_checkpoint(filename=ckpt, save_best_only=True, save_weights_only=False)
 
     cnn.fit(epochs=get_epochs(check), x_train=x_train, y_train=y_train, batch_size=128, shuffle=True, extract_validation_dataset=True)
+    time_in_seconds = time.time() - start_time
 
     cnn.save_loss_plot(os.path.join(config['output_path'], f'cnn_loss.png'))
 
-def train_autoencoder(file: str, check: bool):
+    append_in_csv(config['time_file'], f'cnn, {time_in_seconds}')
+
+def train_autoencoder(file: str, check: bool, config: Dict[str, str]):
+    start_time = time.time()
     from denoising.methods.neural_network.denoising_autoencoder import DenoisingAutoencoder
+
+    (x_train, y_train, _, _) = load_data(check)
 
     ae = DenoisingAutoencoder(image_dimension=(52,52))
     ae.compile(optimizer='adam', learning_rate=1e-3, loss='mse')
@@ -102,25 +119,33 @@ def train_autoencoder(file: str, check: bool):
 
     ae.fit(epochs=get_epochs(check), x_train=x_train, y_train=y_train, batch_size=128, shuffle=True, extract_validation_dataset=True)
 
+    time_in_seconds = time.time() - start_time
     ae.save_loss_plot(os.path.join(config['output_path'], f'Autoencoder_loss.png'))
 
-def train_cgan(file: str, check: bool):
+    append_in_csv(config['time_file'], f'autoencoder, {time_in_seconds}')
+
+
+def train_cgan(file: str, check: bool, config: Dict[str, str]):
+    start_time = time.time()
     from denoising.methods.neural_network.cgan_denoiser.main import CGanDenoiser
 
     (x_train, y_train, _, _) = load_data(check)
-    config = load_config(file)
     
     gan = CGanDenoiser(image_dimensions=(52,52))
     gan.compile(optimizer='adam', learning_rate=0.0001, loss='mse')
     gan.set_checkpoint(directory=os.path.join(config['metadata_path'], 'cgan-ckpt'))
 
     gan.fit(epochs=get_epochs(check), x_train=x_train, y_train=y_train, batch_size=256)
+    time_in_seconds = time.time() - start_time
 
-def train_dncnn(file: str, check: bool):
+    append_in_csv(config['time_file'], f'gan, {time_in_seconds}')
+
+
+def train_dncnn(file: str, check: bool, config: Dict[str, str]):
+    start_time = time.time()
     from denoising.methods.neural_network.dncnn import DnCNN
 
     (x_train, y_train, _, _) = load_data(check)
-    config = load_config(file)
 
     dncnn = DnCNN(number_of_layers=19)
     dncnn.compile(optimizer="adam", learning_rate=0.0001, loss='mse')
@@ -129,10 +154,18 @@ def train_dncnn(file: str, check: bool):
     dncnn.set_checkpoint(filename=ckpt, save_best_only=True, save_weights_only=False)
     dncnn.fit(epochs=get_epochs(check), x_train=x_train, y_train=y_train, batch_size=128, shuffle=True, extract_validation_dataset=True)
 
+    time_in_seconds = time.time()
     dncnn.save_loss_plot(os.path.join(config['output_path'], f'DnCNN_loss.png'))
 
+    append_in_csv(config['time_file'], f'dncnn, {time_in_seconds}')
+
 def train(filename: str, check: bool):
+    config = load_config(filename)
+
+    config['time_file'] = os.path.join(config['metadata_path'], 'time.csv')
+    append_in_csv(config['time_file'], 'name, seconds') 
+
     for func in [train_mlp, train_cnn, train_autoencoder, train_cgan, train_dncnn]:
-        p = Process(target=func, args=(filename, check, ))
+        p = Process(target=func, args=(filename, check, config, ))
         p.start()
         p.join()
